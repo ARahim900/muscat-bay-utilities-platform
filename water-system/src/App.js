37155de40a43d483c7193246929996d2260631f9
Abdularahim Al Balushi <143422547+ARahim900@users.noreply.github.com>
2025-05-06 23:37:15 +0400

    Add water system App.js

---
+++ water-system/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MuscatBayWaterSystem from './components/MuscatBayWaterSystem';
+
+function App() {
+  return (
+    <div className="App">
+      <MuscatBayWaterSystem />
+    </div>
+  );
+}
+
+export default App;
